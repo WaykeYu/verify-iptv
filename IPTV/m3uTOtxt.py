2d4127a1b44a705d80c0f76616584c1f69676f37
WaykeYu <90821094+WaykeYu@users.noreply.github.com>
2025-02-14 16:52:52 +0800

    Update m3uTOtxt.py

--- IPTV/m3uTOtxt.py
+++ IPTV/m3uTOtxt.py
@@ -12,13 +12,14 @@ def get_all_m3u_files(base_url):
             href = a['href']
             if href.endswith('.m3u') and 'blob' in href:
                 raw_url = href.replace('/blob/', '/raw/')
-                m3u_files.append(("https://github.com" + raw_url, href))
+                file_name = os.path.basename(href)
+                m3u_files.append(("https://github.com" + raw_url, file_name))
         return m3u_files
     except requests.exceptions.RequestException as e:
         print(f"獲取 .m3u 文件列表失敗: {e}")
         return []
 
-def download_and_convert(url, original_path):
+def download_and_convert(url, file_name):
     try:
         response = requests.get(url, timeout=10)
         response.raise_for_status()
@@ -27,12 +28,11 @@ def download_and_convert(url, original_path):
             print(f"{url} 文件內容為空")
             return
 
-        txt_content = content.replace(".m3u", ".txt")
-        new_path = original_path.replace(".m3u", ".txt")
+        txt_path = file_name.replace(".m3u", ".txt")
 
-        with open(new_path, 'w', encoding='utf-8') as file:
-            file.write(txt_content)
-        print(f"已將 {original_path} 轉換並保存為 {new_path}")
+        with open(txt_path, 'w', encoding='utf-8') as file:
+            file.write(content)
+        print(f"已將 {file_name} 轉換並保存為 {txt_path}")
 
     except requests.exceptions.RequestException as e:
         print(f"下載或轉換文件失敗: {e}")
@@ -41,8 +41,8 @@ def main():
     base_url = "https://github.com/WaykeYu/verify-iptv"
     m3u_files = get_all_m3u_files(base_url)
 
-    for url, original_path in m3u_files:
-        download_and_convert(url, original_path)
+    for url, file_name in m3u_files:
+        download_and_convert(url, file_name)
 
 if __name__ == "__main__":
     main()
